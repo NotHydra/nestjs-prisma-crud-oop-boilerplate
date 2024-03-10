64f5381ff553616eebc4ee428dd4f6fce8bfa7f1
NotHydra <rizky.irswanda115@gmail.com>
2024-03-10 18:15:53 +0800

    feat: author dto format validation handling

--- source/model/author/author.service.ts
+++ source/model/author/author.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, InternalServerErrorException } from "@nestjs/common";
+
+import { Override } from "../../common/decorator/override";
 
 import { ExtendService } from "../../global/extend.service";
 
@@ -15,5 +17,51 @@ export class AuthorService
 {
     constructor(prismaService: PrismaService) {
         super(AuthorService.name, prismaService.author, { book: true });
+    }
+
+    @Override
+    public async add(payload: AuthorCreateDTO): Promise<AuthorModel> {
+        try {
+            if (!/^[a-zA-Z0-9_]+$/.test(payload.name)) {
+                throw new BadRequestException(`Name Must Contain Only Letters, Numbers, and Underscores`);
+            }
+
+            if (payload.name.length < 3) {
+                throw new BadRequestException(`Name Must Be At Least 3 Characters Long`);
+            }
+        } catch (error) {
+            if (error instanceof BadRequestException) {
+                this.loggerService.error(`Add: ${error.message}`);
+                throw error;
+            }
+
+            this.loggerService.error(`Add: ${error.message}`);
+            throw new InternalServerErrorException("Internal Server Error");
+        }
+
+        return super.add(payload);
+    }
+
+    @Override
+    public async change(id: number, payload: AuthorUpdateDTO): Promise<AuthorModel> {
+        try {
+            if (!/^[a-zA-Z0-9_]+$/.test(payload.name)) {
+                throw new BadRequestException(`Name Must Contain Only Letters, Numbers, and Underscores`);
+            }
+
+            if (payload.name.length < 3) {
+                throw new BadRequestException(`Name Must Be At Least 3 Characters Long`);
+            }
+        } catch (error) {
+            if (error instanceof BadRequestException) {
+                this.loggerService.error(`Change: ${error.message}`);
+                throw error;
+            }
+
+            this.loggerService.error(`Change: ${error.message}`);
+            throw new InternalServerErrorException("Internal Server Error");
+        }
+
+        return super.change(id, payload);
     }
 }
